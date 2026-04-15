0b2f9e205ec39898977401a21b93a5bcee3af93c
Igor Li <igoli2@yandex.ru>
2026-04-15 11:45:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telebot import TeleBot, types
 from faker import Faker
 
 
-bot = TeleBot(token='8759454632:AAGEtGi0qEtuC6teLOl6fLjmNQPAEwFpl04', parse_mode='html') # создание бота
+bot = TeleBot(token='Вставить_свой_токен', parse_mode='html') # создание бота
 
 faker = Faker() # утилита для генерации номеров кредитных карт
 
